458e42a0bf498885badf34583f0f4a947b2f4acb
RosyG <rmga2106@gmail.com>
2017-11-30 23:46:30 -0600

    maquetado para mostrar y ocultar imagenes

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+function mostrar() {
+  /*document.getElementsByt('img-cabin').style.visibility='visible';*/
+  document.getElementById('id').style.visibility='visible';
+  /*alert ("visible");*/
+}
+
+function ocultar() {
+  /*document.getElementsByClassName('img-cake').style.visibility='hidden';*/
+  document.getElementById('id').style.visibility='visible';
+  /*console.log("invisible");*/
+}
